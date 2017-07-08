924d4d43d8df6e12f1ccd007f47c1905e896b0f3
Simi Mutasa <stmutasa@gmail.com>
2017-07-07 23:44:51 -0400

    Added ST network, stanford style residual

--- SODNetwork.py
+++ SODNetwork.py
@@ -75,7 +75,7 @@ class SODMatrix():
             return conv
 
 
-    def convolution_3d(self, scope, X, F, K, S=2, padding='SAME', phase_train=None, summary=True, BN=True):
+    def convolution_3d(self, scope, X, F, K, S=2, padding='SAME', phase_train=None, summary=True, BN=True, relu=True):
         """
         This is a wrapper for 3-dimensional convolutions
         :param scope:
@@ -117,7 +117,7 @@ class SODMatrix():
                                                             scope=scope, decay=0.9, epsilon=1e-5))
 
             # Relu activation
-            conv = tf.nn.relu(conv, name=scope.name)
+            if relu: conv = tf.nn.relu(conv, name=scope.name)
 
             # Create a histogram/scalar summary of the conv1 layer
             if summary: self._activation_summary(conv)
@@ -125,7 +125,7 @@ class SODMatrix():
             return conv
 
 
-    def depthwise_convolution(self, scope, X, F, K, S=2, padding='SAME', phase_train=None, summary=True, BN=True):
+    def depthwise_convolution(self, scope, X, F, K, S=2, padding='SAME', phase_train=None, summary=True, BN=True, relu=True):
             """
             This is a wrapper for depthwise convolutions
             :param scope:
@@ -171,7 +171,7 @@ class SODMatrix():
                                                                         scope=scope, decay=0.9, epsilon=1e-5))
 
                 # Relu activation
-                conv = tf.nn.relu(conv, name=scope.name)
+                if relu: conv = tf.nn.relu(conv, name=scope.name)
 
                 # Create a histogram/scalar summary of the conv1 layer
                 if summary: self._activation_summary(conv)
@@ -180,7 +180,7 @@ class SODMatrix():
 
 
     def deconvolution(self, scope, X, F, K, S, padding='SAME', phase_train=None,
-                      concat_var=None, out_shape=None, summary=True, BN=True):
+                      concat_var=None, out_shape=None, summary=True, BN=True, relu=True):
         """
         This is a wrapper for De-convolutions aka fractionally strided convolutions aka transposed convolutions
         aka upconvolutions aka backwards convolutions
@@ -232,7 +232,7 @@ class SODMatrix():
                                                                     scope=scope, decay=0.9, epsilon=1e-5))
 
             # Relu
-            conv = tf.nn.relu(conv, name=scope.name)
+            if relu: conv = tf.nn.relu(conv, name=scope.name)
 
             # Create a histogram summary and summary of sparsity
             if summary: self._activation_summary(conv)
@@ -287,7 +287,7 @@ class SODMatrix():
             return inception
 
 
-    def incepted_downsample(self, scope, X, K, S=2, padding='SAME', phase_train=None, summary=True, BN=True):
+    def incepted_downsample(self, scope, X, K, S=2, padding='SAME', phase_train=None, summary=True, BN=True, relu=True):
         """
         This function implements a downsampling layer that gives the network 3 options
          for reducing parameter size, maxPool, avgPool or strided convolutions
@@ -335,7 +335,7 @@ class SODMatrix():
                                                                         scope=scope, decay=0.9, epsilon=1e-5))
 
             # Relu activation
-            inception = tf.nn.relu(inception, name=scope.name)
+            if relu: inception = tf.nn.relu(inception, name=scope.name)
 
             # Create a histogram/scalar summary of the conv1 layer
             if summary: self._activation_summary(inception)
@@ -343,7 +343,7 @@ class SODMatrix():
             return inception
 
 
-    def residual_layer(self, scope, X, F, K, padding='SAME', phase_train=None, summary=True, BN=True):
+    def res_inc_layer(self, scope, X, F, K, padding='SAME', phase_train=None, summary=True, BN=True, relu=True):
         """
         This is a wrapper for implementing a hybrid residual layer with inception layer as F(x)
         :param scope:
@@ -392,12 +392,56 @@ class SODMatrix():
                                                             scope=scope, decay=0.9, epsilon=1e-5))
 
             # Relu activation
-            conv = tf.nn.relu(residual, name=scope.name)
+            if relu: conv = tf.nn.relu(residual, name=scope.name)
 
             # Create a histogram/scalar summary of the conv1 layer
             if summary: self._activation_summary(conv)
 
             return conv
+
+
+    def residual_layer(self, scope, X, F, K, K_prob, padding='SAME', phase_train=None, summary=True):
+        """
+        This is a wrapper for implementing a stanford style residual layer
+        :param scope:
+        :param X: Output of the previous layer, make sure this is not normalized and has no nonlinearity applied
+        :param F: Dimensions of the second convolution in F(x) - the non inception layer one
+        :param K: Feature maps in the inception layer (will be multiplied by 4 during concatenation)
+        :param k_prob: keep probability for dropout
+        :param padding: SAME or VALID
+        :param phase_train: For batch norm implementation
+        :param summary: whether to produce a tensorboard summary of this layer
+        :return:
+        """
+
+        # Set the scope. Implement a residual layer below
+        with tf.variable_scope(scope) as scope:
+
+            # Start the second conv layer: BN->Relu->Drop->Conv->BN->Relu->dropout
+            conv1 = self.batch_normalization(X, phase_train, scope)
+
+            # ReLU
+            conv1 = tf.nn.relu(conv1, scope.name)
+
+            # Dropout
+            conv1 = tf.nn.dropout(conv1, K_prob)
+
+            # Another Convolution with BN and ReLu
+            conv2 = self.convolution(scope, conv1, F, K, 1, 'SAME', phase_train, summary, True, True)
+
+            # Second dropout
+            conv2 = tf.nn.dropout(conv2, K_prob)
+
+            # Final STRIDED conv without BN or RELU
+            conv = self.convolution(scope, conv2, F, K, 2, 'SAME', phase_train, summary, False, False)
+
+            # Max pool of the input convolution
+            pool = tf.nn.max_pool(X, [1, F, F, 1], [1, 2, 2, 1], padding)
+
+            # The Residual block
+            residual = tf.add(conv, pool)
+
+            return residual
 
 
     def _activation_summary(self, x):
@@ -505,6 +549,46 @@ class SODMatrix():
             return linear
 
 
+    def spatial_transform_layer(self, scope, X):
+        """
+        Spatial transformer network implementation
+        :param scope: Scope
+        :param X: The input tensor
+        :return: h_trans: the output of the transformer
+        """
+
+        dim = X.get_shape().as_list()[1]
+        batch_size = X.get_shape().as_list()[0]
+
+        with tf.variable_scope(scope) as scope:
+
+            # Set up the localisation network to calculate floc(u):
+            W1 = tf.get_variable('Weights1', shape=[dim * dim * 128, 20],
+                                 initializer=tf.truncated_normal_initializer(stddev=5e-2))
+            B1 = tf.get_variable('Bias1', shape=[20], initializer=tf.truncated_normal_initializer(stddev=5e-2))
+            W2 = tf.get_variable('Weights2', shape=[20, 6], initializer=tf.truncated_normal_initializer(stddev=5e-2))
+
+            # Add weights to collection
+            tf.add_to_collection('weights', W1)
+            tf.add_to_collection('weights', W2)
+
+            # Always start with the identity transformation
+            initial = np.array([[1.0, 0, 0], [0, 1.0, 0]])
+            initial = initial.astype('float32')
+            initial = initial.flatten()
+            B2 = tf.Variable(initial_value=initial, name='Bias2')
+
+            # Define the two layers of the localisation network
+            H1 = tf.nn.tanh(tf.matmul(tf.zeros([batch_size, 16 * 16 * 128]), W1) + B1)
+            H2 = tf.nn.tanh(tf.matmul(H1, W2) + B2)
+
+            # Define the output size to the original dimensions
+            output_size = (dim, dim)
+            h_trans = self.transformer(X, H2, output_size)
+
+            return h_trans
+
+
     """
          Loss function wrappers
     """
@@ -598,3 +682,213 @@ class SODMatrix():
         tf.add_to_collection('losses', loss)
 
         return loss
+
+
+    """
+            Utility Functions
+    """
+
+    def batch_normalization(self, conv, phase_train, scope):
+        """
+        Wrapper for the batch normalization implementation
+        :param conv: The layer to normalize
+        :param phase_train: Is this training or testing phase
+        :param scope: the scope for this operation
+        :return: conv: the normalized convolution
+        """
+
+        return tf.cond(phase_train,
+                       lambda: tf.contrib.layers.batch_norm(conv, activation_fn=None, center=True, scale=True,
+                                                    updates_collections=None, is_training=True, reuse=None,
+                                                    scope=scope, decay=0.9, epsilon=1e-5),
+                       lambda: tf.contrib.layers.batch_norm(conv, activation_fn=None, center=True, scale=True,
+                                                    updates_collections=None, is_training=False, reuse=True,
+                                                    scope=scope, decay=0.9, epsilon=1e-5))
+
+
+    def transformer(self, U, theta, out_size, name='SpatialTransformer', **kwargs):
+        """Spatial Transformer Layer
+
+        Implements a spatial transformer layer as described in [1]_.
+        Based on [2]_ and edited by David Dao for Tensorflow.
+
+        Parameters
+        ----------
+        U : float
+            The output of a convolutional net should have the
+            shape [num_batch, height, width, num_channels].
+        theta: float
+            The output of the
+            localisation network should be [num_batch, 6].
+        out_size: tuple of two ints
+            The size of the output of the network (height, width)
+
+        References
+        ----------
+        .. [1]  Spatial Transformer Networks
+                Max Jaderberg, Karen Simonyan, Andrew Zisserman, Koray Kavukcuoglu
+                Submitted on 5 Jun 2015
+        .. [2]  https://github.com/skaae/transformer_network/blob/master/transformerlayer.py
+
+        Notes
+        -----
+        To initialize the network to the identity transform init
+        ``theta`` to :
+            identity = np.array([[1., 0., 0.],
+                                 [0., 1., 0.]])
+            identity = identity.flatten()
+            theta = tf.Variable(initial_value=identity)
+
+        """
+
+        def _repeat(x, n_repeats):
+            with tf.variable_scope('_repeat'):
+                rep = tf.transpose(
+                    tf.expand_dims(tf.ones(shape=tf.stack([n_repeats, ])), 1), [1, 0])
+                rep = tf.cast(rep, 'int32')
+                x = tf.matmul(tf.reshape(x, (-1, 1)), rep)
+                return tf.reshape(x, [-1])
+
+        def _interpolate(im, x, y, out_size):
+            with tf.variable_scope('_interpolate'):
+                # constants
+                num_batch = im.get_shape().as_list()[0]
+                height = im.get_shape().as_list()[1]
+                width = im.get_shape().as_list()[2]
+                channels = im.get_shape().as_list()[3]
+
+                x = tf.cast(x, 'float32')
+                y = tf.cast(y, 'float32')
+                height_f = tf.cast(height, 'float32')
+                width_f = tf.cast(width, 'float32')
+                out_height = out_size[0]
+                out_width = out_size[1]
+                zero = tf.zeros([], dtype='int32')
+                max_y = tf.cast(im.get_shape().as_list()[1] - 1, 'int32')
+                max_x = tf.cast(im.get_shape().as_list()[2] - 1, 'int32')
+
+                # scale indices from [-1, 1] to [0, width/height]
+                x = (x + 1.0) * (width_f) / 2.0
+                y = (y + 1.0) * (height_f) / 2.0
+
+                # do sampling
+                x0 = tf.cast(tf.floor(x), 'int32')
+                x1 = x0 + 1
+                y0 = tf.cast(tf.floor(y), 'int32')
+                y1 = y0 + 1
+
+                x0 = tf.clip_by_value(x0, zero, max_x)
+                x1 = tf.clip_by_value(x1, zero, max_x)
+                y0 = tf.clip_by_value(y0, zero, max_y)
+                y1 = tf.clip_by_value(y1, zero, max_y)
+                dim2 = width
+                dim1 = width * height
+                base = _repeat(tf.range(num_batch) * dim1, out_height * out_width)
+                base_y0 = base + y0 * dim2
+                base_y1 = base + y1 * dim2
+                idx_a = base_y0 + x0
+                idx_b = base_y1 + x0
+                idx_c = base_y0 + x1
+                idx_d = base_y1 + x1
+
+                # use indices to lookup pixels in the flat image and restore
+                # channels dim
+                im_flat = tf.reshape(im, tf.stack([-1, channels]))
+                im_flat = tf.cast(im_flat, 'float32')
+                Ia = tf.gather(im_flat, idx_a)
+                Ib = tf.gather(im_flat, idx_b)
+                Ic = tf.gather(im_flat, idx_c)
+                Id = tf.gather(im_flat, idx_d)
+
+                # and finally calculate interpolated values
+                x0_f = tf.cast(x0, 'float32')
+                x1_f = tf.cast(x1, 'float32')
+                y0_f = tf.cast(y0, 'float32')
+                y1_f = tf.cast(y1, 'float32')
+                wa = tf.expand_dims(((x1_f - x) * (y1_f - y)), 1)
+                wb = tf.expand_dims(((x1_f - x) * (y - y0_f)), 1)
+                wc = tf.expand_dims(((x - x0_f) * (y1_f - y)), 1)
+                wd = tf.expand_dims(((x - x0_f) * (y - y0_f)), 1)
+                output = tf.add_n([wa * Ia, wb * Ib, wc * Ic, wd * Id])
+                return output
+
+        def _meshgrid(height, width):
+            with tf.variable_scope('_meshgrid'):
+                # This should be equivalent to:
+                #  x_t, y_t = np.meshgrid(np.linspace(-1, 1, width),
+                #                         np.linspace(-1, 1, height))
+                #  ones = np.ones(np.prod(x_t.shape))
+                #  grid = np.vstack([x_t.flatten(), y_t.flatten(), ones])
+                x_t = tf.matmul(tf.ones(shape=tf.stack([height, 1])),
+                                tf.transpose(tf.expand_dims(tf.linspace(-1.0, 1.0, width), 1), [1, 0]))
+                y_t = tf.matmul(tf.expand_dims(tf.linspace(-1.0, 1.0, height), 1),
+                                tf.ones(shape=tf.stack([1, width])))
+
+                x_t_flat = tf.reshape(x_t, (1, -1))
+                y_t_flat = tf.reshape(y_t, (1, -1))
+
+                ones = tf.ones_like(x_t_flat)
+                grid = tf.concat(axis=0, values=[x_t_flat, y_t_flat, ones])
+                return grid
+
+        def _transform(theta, input_dim, out_size):
+            with tf.variable_scope('_transform'):
+                num_batch = input_dim.get_shape().as_list()[0]
+                height = input_dim.get_shape().as_list()[1]
+                width = input_dim.get_shape().as_list()[2]
+                num_channels = input_dim.get_shape().as_list()[3]
+                theta = tf.reshape(theta, (-1, 2, 3))
+                theta = tf.cast(theta, 'float32')
+
+                # grid of (x_t, y_t, 1), eq (1) in ref [1]
+                height_f = tf.cast(height, 'float32')
+                width_f = tf.cast(width, 'float32')
+                out_height = out_size[0]
+                out_width = out_size[1]
+                grid = _meshgrid(out_height, out_width)
+                grid = tf.expand_dims(grid, 0)
+                grid = tf.reshape(grid, [-1])
+                grid = tf.tile(grid, tf.stack([num_batch]))
+                grid = tf.reshape(grid, tf.stack([num_batch, 3, -1]))
+
+                # Transform A x (x_t, y_t, 1)^T -> (x_s, y_s)
+                T_g = tf.matmul(theta, grid)
+                x_s = tf.slice(T_g, [0, 0, 0], [-1, 1, -1])
+                y_s = tf.slice(T_g, [0, 1, 0], [-1, 1, -1])
+                x_s_flat = tf.reshape(x_s, [-1])
+                y_s_flat = tf.reshape(y_s, [-1])
+
+                input_transformed = _interpolate(
+                    input_dim, x_s_flat, y_s_flat,
+                    out_size)
+
+                output = tf.reshape(
+                    input_transformed, tf.stack([num_batch, out_height, out_width, num_channels]))
+                return output
+
+        with tf.variable_scope(name):
+            output = _transform(theta, U, out_size)
+            return output
+
+
+    def batch_transformer(self, U, thetas, out_size, name='BatchSpatialTransformer'):
+        """Batch Spatial Transformer Layer
+
+        Parameters
+        ----------
+
+        U : float
+            tensor of inputs [num_batch,height,width,num_channels]
+        thetas : float
+            a set of transformations for each input [num_batch,num_transforms,6]
+        out_size : int
+            the size of the output [out_height,out_width]
+
+        Returns: float
+            Tensor of size [num_batch*num_transforms,out_height,out_width,num_channels]
+        """
+        with tf.variable_scope(name):
+            num_batch, num_transforms = map(int, thetas.get_shape().as_list()[:2])
+            indices = [[i] * num_transforms for i in range(num_batch)]
+            input_repeated = tf.gather(U, tf.reshape(indices, [-1]))
+            return self.transformer(input_repeated, thetas, out_size)
